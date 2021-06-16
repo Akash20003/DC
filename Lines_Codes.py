b85885423f0c35d4dacd90f9378d5eac3662460a
Akash Kulkarni <83762261+Akash20003@users.noreply.github.com>
2021-06-16 20:36:59 +0530

    Comments Added

--- Lines_Codes.py
+++ Lines_Codes.py
@@ -1,6 +1,11 @@
+'Line Codes'
+'Uni-Polar, Bi-Polar, Polar NRZ and RZ'
+#Imports Matplotlib to plot the Graph
 import matplotlib.pyplot as plt
+#Imports Numpy Lib
 import numpy as np
 
+#Accepts Inputs from User
 N = int(input("Number of Bits to be Transmitted : "))
 y = []
 y.append(0)
@@ -8,6 +13,7 @@ for n in range(0, N):
     x = int(input(""))
     y.append(x)
 
+#This Function returns Polar NRZ Values
 def pol():
     y1 = []
     for i in y:
@@ -15,6 +21,7 @@ def pol():
         y1.append(i)
     return y1
 
+#This Function returns Bi-Polar NRZ Values
 def bi():
     y2 = []
     a = 0
@@ -26,6 +33,7 @@ def bi():
         y2.append(i)
     return y2
 
+#This Function returns Uni-Polar RZ Values
 def uni_rz():
     y3 = []
     for i in y:
@@ -36,6 +44,7 @@ def uni_rz():
     y3.pop(0)
     return y3
 
+#This Function returns Polar RZ Values
 def pol_rz():
     y4 = []
     for i in y:
@@ -50,6 +59,7 @@ def pol_rz():
     y5.pop(0)
     return y5
 
+#This Function returns Bi-Polar RZ Values
 def bi_rz():
     y6 = []
     a = 0
@@ -68,6 +78,7 @@ def bi_rz():
     y7.pop(0)
     return y7
 
+#This Function returns Titles and etc to plot the Graph
 def titles(m):
     plt.subplot(3, 1, m)
     plt.xlabel('Time')
@@ -75,42 +86,53 @@ def titles(m):
     plt.tight_layout()
     plt.grid()
 
+#Plotting Starts :
+
+#Figure 1 with x-axis
 plt.figure(1)
 x = range(0, N+1)
 
+#Plots Uni-Polar NRZ
 titles(1)
 plt.step(x, y)
 plt.title('Uni-Polar NRZ')
 plt.xticks(x)
 
+#Plots Polar NRZ
 titles(2)
 plt.step(x, pol(), color='g')
 plt.title('Polar NRZ')
 plt.xticks(x)
 
+#Plots Bi-Polar NRZ
 titles(3)
 plt.step(x, bi(), color='r')
 plt.title('Bi-Polar NRZ')
 plt.xticks(x)
 
+#Figure 2 with Change in x-axis
 plt.figure(2)
 x1 = np.arange(0, (N+2)-1.5, 0.5)
 
+#Plots Uni-Polar RZ
 titles(1)
 plt.step(x1, uni_rz(), color='purple')
 plt.xticks(x1)
 plt.title('Uni-Polar RZ')
 
+#Plots Polar RZ
 titles(2)
 plt.step(x1, pol_rz(), color='c')
 plt.xticks(x1)
 plt.title('Polar RZ')
 
+#Plots Bi-Polar RZ
 titles(3)
 plt.step(x1, bi_rz(), color='m')
 plt.xticks(x1)
 plt.title('Bi-Polar RZ')
 
+#Shows the Graph
 plt.show()
 
 print("Programmed by Akash U Kulkarni")
